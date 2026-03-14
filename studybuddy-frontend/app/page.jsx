7b34fe9f8e295c6f9e59518fa1d60280095b934a
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-03-14 13:00:51 +0300

    Added Login and Register buttons to home page

--- studybuddy-frontend/app/page.jsx
+++ studybuddy-frontend/app/page.jsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<>
-			<h1 className="text-4xl">Study Buddy</h1>
-		</>
+		<div className="page">
+			<h1 className='title'>Welcome to Study Buddy!</h1>
+			<h2 className="sub-title">Login or Create an account</h2>
+			<Link href="/login">
+				<button className="mb-4 text-2xl">Login</button>
+			</Link>
+			<Link href="/register">
+				<button className="text-2xl">Register</button>
+			</Link>
+		</div>
 	);
 }
