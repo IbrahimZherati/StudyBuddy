103490f975784ddfa75cbbff72681abb30edfafc
Randa Mohammad <r80817734@gmail.com>
2026-03-15 16:32:13 +0300

    Adjusting the button position in GoBackButton component

--- studybuddy-frontend/components/GoBackButton.jsx
+++ studybuddy-frontend/components/GoBackButton.jsx
@@ -6,7 +6,7 @@ import {useRouter} from "next/navigation"
 export default function GoBackButton() {
     const x = useRouter()
   return (
-    <div className='absolute top-8 left-12'>
+    <div className='absolute top-8 left-[25%]'>
         <button onClick={()=>x.back()} className='border-2 backdrop-blur-md p-1 rounded-full text-white cursor-pointer'>
             <ArrowLeft size={20}/>
         </button>
