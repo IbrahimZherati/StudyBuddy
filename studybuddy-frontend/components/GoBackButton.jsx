22c48e414f11d2ae35495fcf94e34e0995c3b8c5
Randa Mohammad <r80817734@gmail.com>
2026-03-18 22:50:08 +0300

    edit GoBackButton potision

--- studybuddy-frontend/components/GoBackButton.jsx
+++ studybuddy-frontend/components/GoBackButton.jsx
@@ -6,10 +6,10 @@ import {useRouter} from "next/navigation"
 export default function GoBackButton() {
     const router = useRouter();
     return (
-      <div className='absolute top-[10vh] left-[30vw]'>
+      <div className='absolute top-6 left-60'>
           <button onClick={() => router.back()} 
-                  className='border-2 backdrop-blur-md p-1 rounded-full text-white cursor-pointer'>
-              <ArrowLeft size={20}/>
+                  className='border-3 backdrop-blur-md p-1 rounded-full text-white cursor-pointer'>
+              <ArrowLeft size={22}/>
           </button>
       </div>
     )
