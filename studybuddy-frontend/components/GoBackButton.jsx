018498990f7bf3a629acb663384470a17048cd85
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-03-20 21:35:41 +0300

    Reduced the size of Login checkbox

--- studybuddy-frontend/components/GoBackButton.jsx
+++ studybuddy-frontend/components/GoBackButton.jsx
@@ -6,12 +6,12 @@ import { useRouter } from "next/navigation"
 export default function GoBackButton() {
     const router = useRouter();
     return (
-      <div className='absolute -top-1 -left-15'>
-          <button onClick={() => router.back()} 
-                  className='border-4 backdrop-blur-md p-1 rounded-full text-white cursor-pointer
-                            hover:scale-[110%] active:text-blue-300 transition duration-200'>
-              <ArrowLeft size={22} strokeWidth={3}/>
-          </button>
-      </div>
+        <div className='absolute -top-1 -left-15'>
+            <button onClick={() => router.back()} 
+                    className='border-4 backdrop-blur-md p-1 rounded-full text-white cursor-pointer
+                                hover:scale-[110%] active:text-blue-300 transition duration-200'>
+                <ArrowLeft size={22} strokeWidth={3}/>
+            </button>
+        </div>
     )
 }
