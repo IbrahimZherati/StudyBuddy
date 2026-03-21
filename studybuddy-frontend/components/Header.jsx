f78197f50fbcd3edb2d77d634f7cd05dad1acb42
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-03-21 18:41:55 +0300

    Applied some changes to Header.jsx

--- studybuddy-frontend/components/Header.jsx
+++ studybuddy-frontend/components/Header.jsx
@@ -4,8 +4,8 @@ import { GraduationCap } from 'lucide-react'
 
 export default function Header() {
     return (
-		<header className='flex justify-evenly items-center py-4 border-0 shadow-lg bg-[#F5F6FF]'>
-			<div className='flex gap-2 '>
+		<header className='flex-row-center justify-evenly py-4 border-0 shadow-lg bg-[#F5F6FF]'>
+			<div className='flex gap-2'>
 				<GraduationCap className='text-[#002CFF] w-9 h-9'/>
 				
 				<h1 className='font-bold text-2xl'>
@@ -13,14 +13,14 @@ export default function Header() {
 				</h1>
 			</div>
 
-			<div className='flex gap-11 items-center'>
-				<Link href="">Free Tools</Link>
+			<div className='flex-row-center gap-11'>
+				<Link href=""><span className='nav-link'>Free Tools</span></Link>
 
-				<Link href="">Community Features</Link>
+				<Link href=""><span className='nav-link'>Community Features</span></Link>
 
-				<Link href="">How it Works</Link>
+				<Link href=""><span className='nav-link'>How it Works</span></Link>
 
-				<Link href="">Reviews</Link>
+				<Link href=""><span className='nav-link'>Reviews</span></Link>
 
 				<Link href="/register">
 					<button className='btn-sign'>
