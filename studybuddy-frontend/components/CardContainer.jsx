26a9bc5e356b3abf21298678f9c6e5598f6391dd
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-03-23 08:29:53 +0300

    Fixed CardContainer.jsx warning

--- studybuddy-frontend/components/CardContainer.jsx
+++ studybuddy-frontend/components/CardContainer.jsx
@@ -1,7 +1,7 @@
 
 export default function CardContainer({children, additionalStyles}) {
     return (
-        <div className={`grid gap-5 [grid-template-columns:repeat(auto-fit,minmax(250px,1fr))]
+        <div className={`grid gap-5 grid-cols-[repeat(auto-fit,minmax(250px,1fr))]
                         ${additionalStyles}`}>
             {children}
         </div>
