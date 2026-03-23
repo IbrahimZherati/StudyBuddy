d532b8070657d2d4bd741d0a8927b6d36a6ded30
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-03-23 21:13:56 +0300

    Fixed register/login form not being centered in mobile screens

--- studybuddy-frontend/app/(auth)/layout.jsx
+++ studybuddy-frontend/app/(auth)/layout.jsx
@@ -1,10 +1,10 @@
 export default function RootLayout({ children }) {
 	return (
-		<main className="w-full min-h-dvh overflow-y-auto 
+		<main className="w-full min-h-dvh
 						bg-linear-90 from-[#111b71] via-[#7b8eef] to-[#111b71]"
 		>
 
-			<div className="flex justify-center items-start md:items-center w-full min-h-dvh px-4 py-8">
+			<div className="flex-row-center w-full min-h-dvh px-4 py-8">
 				{children}
 			</div>
 		</main>
