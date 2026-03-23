c6e1b79ffc4690af4cc24aba6d15a4c2628af2dd
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-03-23 20:53:59 +0300

    Made the whole feature card clickable

--- studybuddy-frontend/components/FeatureCard.jsx
+++ studybuddy-frontend/components/FeatureCard.jsx
@@ -3,26 +3,28 @@ import Card from "./Card";
 
 export default function FeatureCard({icon, title, desc, href}) {
     return (
-        <Card>
-            <div>
-				{icon}
-			</div>
-
-			<div className="flex flex-col gap-6">
+		<Link href={href}>
+			<Card>
 				<div>
-					<h4 className="text-xl font-semibold">
-						{title}
-					</h4>
-
-					<p className="text-gray-600">
-						{desc}
-					</p>
+					{icon}
 				</div>
 
-				<Link className="text-blue-600 font-semibold mt-auto" href={href}>
-					Try it →
-				</Link>
-			</div>
-        </Card>
+				<div className="flex flex-col gap-6">
+					<div>
+						<h4 className="text-xl font-semibold">
+							{title}
+						</h4>
+
+						<p className="text-gray-600">
+							{desc}
+						</p>
+					</div>
+
+					<p className="text-blue-600 font-semibold mt-auto">
+						Try it →
+					</p>
+				</div>
+			</Card>
+		</Link>
     )
 }
