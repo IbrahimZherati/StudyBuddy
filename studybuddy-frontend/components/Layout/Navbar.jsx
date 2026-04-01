c3f52e8f47b71a0dcd8e236b8093ac46839224c0
Randa Mohammad <r80817734@gmail.com>
2026-04-01 18:42:34 +0300

    make icons as links in Navbar component.  And make Navbar more responsive

--- studybuddy-frontend/components/Layout/Navbar.jsx
+++ studybuddy-frontend/components/Layout/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Home, User, Bell, Folder, MessageCircle, Users, Globe2} from "lucide-react";
+import Link from 'next/link';
 
 export default function Navbar() {
   const userName = { 
@@ -7,16 +8,41 @@ export default function Navbar() {
     img : "https://i.pravatar.cc/40"
 };
   return (
-    <nav className='flex-row-center justify-between gap-4 py-2 px-4 bg-[#F5F6FF] border-b'>
+    <nav className='fixed top-0 left-0 md:left-56 right-0 h-16 z-40
+        flex-row-center justify-between gap-4 py-2 px-4 bg-[#F5F6FF] 
+        border-b pl-18 md:pl-0 whitespace-nowrap
+        overflow-x-auto /* scroll */'
+    >
         
-        <div className='flex-row-center gap-16 '>
-            <Home className='icon-navbar '/>
-            <User className='icon-navbar'/>
-            <Bell className='icon-navbar'/>
-            <Folder className='icon-navbar'/>
-            <MessageCircle className='icon-navbar'/>
-            <Users className='icon-navbar'/>
-            <Globe2 className='icon-navbar'/>
+        <div className='flex-row-center gap-7 md:gap-16  min-w-max '>
+            <Link href="">
+              <Home className='icon-navbar '/>
+            </Link>
+            
+            <Link href="">
+              <User className='icon-navbar'/>
+            </Link>
+            
+            <Link href="">
+              <Bell className='icon-navbar'/>
+            </Link>
+            
+            <Link href="">
+              <Folder className='icon-navbar'/>
+            </Link>
+            
+            <Link href="">
+              <MessageCircle className='icon-navbar'/>
+            </Link>
+            
+            <Link href="">
+              <Users className='icon-navbar'/>
+            </Link>
+            
+            <Link href="">
+              <Globe2 className='icon-navbar'/>
+            </Link>
+            
         </div>
 
         <div className='flex gap-2.5'>
