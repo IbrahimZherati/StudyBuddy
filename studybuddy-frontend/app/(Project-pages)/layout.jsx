b5ae88105e76bbb3bd39a100798502ade807667f
Randa Mohammad <r80817734@gmail.com>
2026-04-01 20:40:07 +0300

    adding gap in Project pages layout

--- studybuddy-frontend/app/(Project-pages)/layout.jsx
+++ studybuddy-frontend/app/(Project-pages)/layout.jsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }) {
         <div className="flex h-screen bg-white">
             <Sidebar />
 
-            <div className="flex-1 flex flex-col">
+            <div className="flex-1 flex flex-col gap-6">
                 <Navbar />
 
                 <main className="md:ml-56 mt-16 p-6">
