40c5935d9101684e2f3ecdc3556f8f0835bfd485
Zherati <Zherati@DESKTOP-CHP9QEK>
2026-04-01 17:08:31 +0300

    update ai promt

--- StudyBuddy.Shared/AI/AiPromt.cs
+++ StudyBuddy.Shared/AI/AiPromt.cs
@@ -13,7 +13,8 @@ namespace StudyBuddy.Shared.AI
         public static string GetGenerateSkillPromt(string bio)
         {
             return $"generate skill tag you think from bio {bio}" +
-                   $" and return skill with coma sperate each";
+                   $" and return skill with coma sperate each and plz not make anything dont say any thing " +
+                   $"just give me skills just best 5 to 8 skill sperate with coma";
         }
 
 
