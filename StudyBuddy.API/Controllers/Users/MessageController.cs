72d41089c0b41e1ee5a7fbaf673fe557c080be1e
Zherati <Zherati@DESKTOP-CHP9QEK>
2026-04-01 16:26:19 +0300

    add message api

---
+++ StudyBuddy.API/Controllers/Users/MessageController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudyBuddy.Application.DTOs.AuthDTOs;
+using StudyBuddy.Application.Services.Auth;
+using StudyBuddy.Application.Services.ClientUsers;
+using StudyBuddy.Application.Services.Messages;
+using StudyBuddy.Shared;
+using StudyBuddy.Shared.DTOs.ClientUserDTO;
+using StudyBuddy.Shared.DTOs.MessageDTO;
+using StudyBuddy.Shared.Enum;
+
+namespace StudyBuddy.API.Controllers.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService MessageService;
+
+        public MessageController(IMessageService MessageService)
+        {
+            this.MessageService = MessageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMessagesForPrivateChat(int FirstClientId, int SecondClientId, int skip = 0, int take = Option.Take , Order orderBy = Order.Desc)
+        {
+            var result = await MessageService.GetMessagesForPrivateChat(FirstClientId , SecondClientId,skip, take , orderBy);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> GetById(int Id)
+        {
+            var result = await MessageService.GetById(Id);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+      
+
+
+
+
+    }
+}
