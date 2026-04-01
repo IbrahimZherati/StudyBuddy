2b132d2856e68ea49f9ff52abd18b97df4f65ec8
Zherati <Zherati@DESKTOP-CHP9QEK>
2026-04-01 12:26:46 +0300

    add clientUser and GroupChat api

---
+++ StudyBuddy.API/Controllers/Users/ClientUserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StudyBuddy.Application.DTOs.AuthDTOs;
+using StudyBuddy.Application.Services.Auth;
+using StudyBuddy.Application.Services.ClientUsers;
+using StudyBuddy.Shared.DTOs.ClientUserDTO;
+
+namespace StudyBuddy.API.Controllers.Users
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ClientUserController : ControllerBase
+    {
+        private readonly IClientUserService clientUserService;
+
+        public ClientUserController(IClientUserService clientUserService)
+        {
+            this.clientUserService = clientUserService;
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateClientUserDTO clientUserDTO)
+        {
+            var result = await clientUserService.Update(clientUserDTO);
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+   
+    }
+}
