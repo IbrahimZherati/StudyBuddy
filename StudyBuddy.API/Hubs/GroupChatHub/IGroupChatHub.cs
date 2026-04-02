c4bb78edca2bc17a73d18b8d4ed8207faa8ec22c
Zherati <Zherati@DESKTOP-CHP9QEK>
2026-04-02 12:06:55 +0300

    add group message controller

---
+++ StudyBuddy.API/Hubs/GroupChatHub/IGroupChatHub.cs
@@ -0,0 +1,14 @@
+﻿using StudyBuddy.Shared.DTOs.GroupMessageDTO;
+using StudyBuddy.Shared.DTOs.MessageDTO;
+using StudyBuddy.Shared.Results;
+
+namespace StudyBuddy.API.Hubs.GroupChatHub
+{
+    public interface IGroupChatHub
+    {
+        Task<Result> JoinGroup(int groupId);
+        Task<Result> LeaveGroup(int groupId);
+        Task<Result> SendMessage(CreateGroupMessageDTO messageDTO);
+
+    }
+}
