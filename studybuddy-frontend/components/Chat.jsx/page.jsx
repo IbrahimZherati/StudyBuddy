6cf7e448be00608a3b72e6800e2443e74da9bf29
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-04-10 16:41:27 +0300

    Created basic chat UI

--- studybuddy-frontend/components/Chat.jsx/page.jsx
+++ studybuddy-frontend/components/Chat.jsx/page.jsx
@@ -2,27 +2,52 @@
 
 import { useChatConnection } from '@/app/hooks/useChatConnection'
 import useGetId from '@/app/hooks/useGetId';
+import { useState } from 'react';
 
 export default function Chat({hubUrlSuffix, to}) {
     const { messages, sendMessage, status } = useChatConnection(hubUrlSuffix);
+    const [text, setText] = useState("");
+    const handleChange = (e) => {
+        setText(e.target.value);
+    }
+
     const id = useGetId();
-    console.log("User Id", id);
+    const handleSend = () => {
+        sendMessage(Number(id), Number(to), text);
+        setText("");
+    };
 
     return (
-        <div>
-            <div className='flex flex-col'>
-                {messages.map((message, index) => 
-                    <div key={index}> 
-                        <span>{`${message.sender}: `}</span>
-                        <span>{message.text}</span>
-                    </div>
-                )}
+        <div className='flex-col-center h-full gap-6'>
+            <div className='flex flex-col w-full h-full'>
+                <div className=''>
+                    {messages.map((message, index) => 
+                        <div key={index}> 
+                            <span className='font-bold'>{`${message.sender}: `}</span>
+                            <span>{message.text}</span>
+                        </div>
+                    )}
+                </div>
             </div>
+
+            <div className='grid grid-cols-[1fr_80px] gap-6 w-full mt-auto'>
+                <input className='border-2 block p-2 rounded-xl bg-tertiary' 
+                    value={text}
+                    placeholder='Type your message here...'
+                    onChange={handleChange}
+                    onKeyDown={(e) => {
+                        if(e.key === 'Enter') {
+                            handleSend();
+                        }
+                    }}
+                />
             
-            <button onClick={() => sendMessage(Number(id), Number(to), "Hi")}
-                    className='btn'>
-                Send
-            </button>
+                <button id="send"
+                        onClick={handleSend}
+                        className='btn place-self-center w-20'>
+                    Send
+                </button>
+            </div>
         </div>
     )
 }
