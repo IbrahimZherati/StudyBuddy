7b584c9a9ae26b1ac36a7e47897cfbac71652704
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-06-11 14:51:59 +0300

    Other users' profiles are done

--- studybuddy-frontend/app/(Project-pages)/Profile/[id]/page.jsx
+++ studybuddy-frontend/app/(Project-pages)/Profile/[id]/page.jsx
@@ -1,37 +1,22 @@
 "use client"
 
 import React from 'react'
-import HeaderProfile from '@/components/Profile/HeaderProfile'
-import Bio from '@/components/Profile/Bio'
-import AvailableDays from '@/components/Profile/AvailableDays'
-import StudyInterests from '@/components/Profile/StudyInterests'
-import FavoriteGroups from '@/components/Profile/FavoriteGroups'
-import BestBuddies from '@/components/Profile/BestBuddies'
-import RecommendedBuddies from '@/components/Profile/RecommendedBuddies'
 import { use } from 'react'
 import useGetOtherUserInfo from '@/app/hooks/useGetOtherUserInfo';
+import Profile from '@/components/Profile/Profile';
+import useGetId from '@/app/hooks/useGetId';
+import Loading from '@/components/Loading';
 
 export default function OtherProfile({ params }) {
     const { id } = use(params); 
-
     const user = useGetOtherUserInfo(id, false);
+    const myId = useGetId();
 
-    return (
-        <div className='flex flex-col gap-6 p-6'>
-            <HeaderProfile isMyProfile={true} />
+    if(!myId)
+        return <Loading />
 
-            <Bio />
-
-            <StudyInterests />
-
-            <AvailableDays />
-
-            <FavoriteGroups />
-
-            <BestBuddies />
-
-            <RecommendedBuddies />
-
-        </div>
-    )
+    return <Profile 
+        user={user}
+        isMyProfile={id == myId}
+    />
 }
