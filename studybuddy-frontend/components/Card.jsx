63545c0bac6073bf5f5e15bc97e27193a6f4180e
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-06-24 22:17:10 +0300

    Fixed cards shadow

--- studybuddy-frontend/components/Card.jsx
+++ studybuddy-frontend/components/Card.jsx
@@ -1,8 +1,8 @@
 
 export default function Card({children, additionalStyles}) {
     return (
-        <div className={`h-full p-4 flex gap-6 rounded-2xl shadow-lg bg-[#F5F6FF]
-                        hover:-translate-y-1 hover:shadow-xl hover:bg-[#FAFAFF]
+        <div className={`h-full p-4 flex gap-6 rounded-2xl shadow-md bg-[#F5F6FF]
+                        hover:-translate-y-1 hover:shadow-lg hover:bg-[#FAFAFF]
                         transition-all duration-300 
                         ${additionalStyles}`}
         >
