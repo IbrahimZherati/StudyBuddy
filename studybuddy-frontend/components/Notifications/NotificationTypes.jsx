87cf9ff0c59e30223a03474264899f90904b05bf
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-06-25 17:56:42 +0300

    Fixed bug with previous commit

--- studybuddy-frontend/components/Notifications/NotificationTypes.jsx
+++ studybuddy-frontend/components/Notifications/NotificationTypes.jsx
@@ -4,8 +4,15 @@ import post from '@/utils/API/post';
 import Link from 'next/link';
 
 const postRequest = async (url, param) => {
-    await post(null, url, param);
-    window.location.reload();
+    try {
+        await post(null, url, param);
+    }
+    catch(error) {
+        console.log(error?.response?.data);
+    }
+    finally {
+        window.location.reload();
+    }
 }
 
 export function FriendRequestNotification({ photo, id, name, time }) {
