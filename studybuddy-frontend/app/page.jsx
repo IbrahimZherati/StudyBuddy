2b4df145382f5b1bc7012eaf1755aab9bf867b3b
Yazan Khalil <yazyazyaz4an@gmail.com>
2026-06-25 17:17:35 +0300

    Now app prevents access to chat page with non-friend users

--- studybuddy-frontend/app/page.jsx
+++ studybuddy-frontend/app/page.jsx
@@ -1,10 +1,8 @@
 import CommunityFeatures from "@/components/HomePage/CommunityFeatures";
 import Footer from "@/components/HomePage/Footer";
-import GuestFeatures from "@/components/HomePage/GuestFeatures";
 import Header from "@/components/HomePage/Header";
 import Hero from "@/components/HomePage/Hero";
 import HowItWorks from "@/components/HomePage/HowItWorks";
-import Review from "@/components/HomePage/Review";
 
 export default function Home() {
 	return (
--- studybuddy-frontend/proxy.js
+++ studybuddy-frontend/proxy.js
@@ -4,16 +4,35 @@ const publicRoutes = ["/", "/login", "/register"];
 
 const isPublicRoute = (route) => publicRoutes.includes(route)
 
-export function proxy(request) {
+export async function proxy(request) {
     const { pathname } = request.nextUrl;
 
     const token = request.cookies.get(".AspNetCore.Identity.Application");
 
-    if(!isPublicRoute(pathname) && !token) {
+    if (!isPublicRoute(pathname) && !token) {
         const loginUrl = new URL("/login", request.url);
         loginUrl.searchParams.set("callbackUrl", pathname);
 
         return NextResponse.redirect(loginUrl);
+    }
+    else if (pathname.startsWith("/chat/")) {
+        const clientId = pathname.split("/").at(-1);
+
+        const response = await fetch(
+            `http://localhost:5203/api/ClientUser/GetProfileByClientId?clientId=${clientId}`, {
+            headers: {
+                Cookie: request.headers.get("cookie") || "",
+            },
+        }
+        );
+
+        const data = await response.json();
+        const userProfile = data.value;
+
+        if (!(userProfile?.isFriend)) {
+            const profileUrl = new URL(`/profile/${clientId}`, request.url);
+            return NextResponse.redirect(profileUrl);
+        }
     }
 
     return NextResponse.next();
