c37a3b4aa53386537b49ce743601e3a961484631
Zherati <ibrahim0949896940@gmail.com>
2026-06-26 12:15:45 +0300

    fix port

--- StudyBuddy.Shared/Helpers/AppHelper.cs
+++ StudyBuddy.Shared/Helpers/AppHelper.cs
@@ -9,7 +9,7 @@ namespace StudyBuddy.Shared.Helpers
     public static class AppHelper
     {
         public static string AppHost = "http://localhost:5203";
-        public static string FrontAppHost = "http://localhost:5000";
+        public static string FrontAppHost = "http://localhost:3000";
 
     }
 }
